d7a13ac4bf715ad2af911566a220fbea3de092ae
wencha <wencha_@hotmail.com>
2020-01-03 10:38:45 -0300

    Update de script de shell

--- bin/scripts/get-cross-refs.sh
+++ bin/scripts/get-cross-refs.sh
@@ -1,9 +1,7 @@
-echo front
-echo back
-echo -------
-for f in `ls lib`
-do echo $f
+for f in `ls lib | egrep -v 'frontend|backend|.*\.js'`
+do echo --- lib/$f
+echo -n Frontend refs
 rgrep "/$f" lib/frontend | wc
+echo -n 'Backend refs '
 rgrep "/$f" lib/backend | wc
-echo ---------
 done
