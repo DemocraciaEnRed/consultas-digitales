f30568be32ed9f948e7a366c3114a0f1b21775c7
wencha <wencha_@hotmail.com>
2020-01-07 13:36:41 -0300

    Algunos cambios de textos en mensajes traídos de Sunchales

--- ext/lib/notifier/translations/index.js
+++ ext/lib/notifier/translations/index.js
@@ -1,15 +1,17 @@
 const translations = require('democracyos-notifier/lib/translations')
 const config = require('lib/config')
+const {protocol, host} = config
+const homeUrl = `${protocol}://${host}`
 
 const t = translations.t
 
 const overrides = {
-  'templates.email.greeting': 'Estimado/a, {userName}',
+  'templates.email.greeting': 'Hola {userName},',
   'templates.email.signature': config.organizationName,
 
   'templates.welcome-email.subject': 'Bienvenido a ' + config.organizationName,
-  'templates.welcome-email.body': 'Para completar su registro haga <a href=\"{validateUrl}\">click aquí.</a>',
-  'templates.welcome-email.ps': 'Si no ha sido usted quien se registró, por favor ignore este correo electrónico.',
+  'templates.welcome-email.body': `Iniciaste el proceso de registro para participar en <a href="${homeUrl}">${config.organizationName}</a>.<br><br>Para finalizarlo, hacé click <a href=\"{validateUrl}\">click aquí.</a>`,
+  'templates.welcome-email.ps': 'En caso de no haberte registrado, por favor ignorá este correo.  ',
 
   'templates.comment-reply.subject': 'Alguien respondió tu comentario',
   'templates.comment-reply.body': 'Tienes una nueva respuesta a tu comentario.',
