4f3f7a1f0b4459cf7cea24591c817b8eba832fda
wencha <wencha_@hotmail.com>
2020-01-13 14:42:22 -0300

    Moviendo header de ext a main

--- ext/lib/boot/overrides.js
+++ ext/lib/boot/overrides.js
@@ -1,9 +1,4 @@
-import 'ext/lib/header/user-badge/component'
-import * as Header from 'lib/frontend/header/component'
-import * as HeaderOverride from 'ext/lib/header/component'
-
 import * as TopicForm from 'lib/frontend/admin/admin-topics-form/view'
 import * as TopicFormOverride from 'ext/lib/admin/admin-topics-form/view'
 
-Object.assign(Header, HeaderOverride)
 Object.assign(TopicForm, TopicFormOverride)
--- ext/lib/header/user-badge/component.js
+++
@@ -1,5 +0,0 @@
-import { UserBadge } from 'lib/frontend/header/user-badge/component'
-
-UserBadge.links = [
-  { label: 'Acerca de este Sitio', path: '/ayuda/acerca' }
-]
