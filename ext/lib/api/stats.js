400977b2c4f8419307f17cd2b6ea5c74182e40ae
Guillermo Croppi <guillermocroppi@gmail.com>
2020-08-28 11:55:35 -0300

    Fix stats - Restricted forums weren't able to get proper stats

--- ext/lib/api/stats.js
+++ ext/lib/api/stats.js
@@ -23,8 +23,9 @@ app.get('/forum/:forumName',
   function getAllForums(req, res, next) {
     Forum
       .findOne({ $or:[ 
-        { name: req.params.forumName, visibility: 'public', "extra.hidden": false },
-        { name: req.params.forumName, visibility: 'collaborative', "extra.hidden": false }
+        // { name: req.params.forumName, visibility: 'public', "extra.hidden": false },
+        // { name: req.params.forumName, visibility: 'collaborative', "extra.hidden": false }
+        { name: req.params.forumName, "extra.hidden": false }
       ]})
       .populate('owner')
       .exec()
