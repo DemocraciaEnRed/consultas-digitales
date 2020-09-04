24be486b54b3b23ab6204aff0c66bf5c78320bba
Guillermo Croppi <guillermocroppi@gmail.com>
2020-09-03 21:59:46 -0300

    sonarcloud being sonarcloud... dont worry

--- ext/lib/notifier/index.js
+++ ext/lib/notifier/index.js
@@ -21,44 +21,44 @@ const interval = setInterval(function () {
 
     // puede ser una de: welcome-email, new-proposal, update-proposal, update-project,
     // subscriber-update-proposal, subscriber-update-project, new-comment o comment-reply
-    const testMailJobs = ['welcome-email', 'forgot-password', 'new-comment', 'comment-reply']
-    const testMailJob = testMailJobs[2]
-    // cuenta a la cual le llegarán los emails
-    const testMailAccount = 'bungew@gmail.com'
-    // id usado para 'subscriber-update-X', tiene que estar en la DB
-    const testMailUserId = '5e3c5cb34324aca40d6727d7'
-    process.env.NOTIFICATIONS_MAILER_EMAIL = testMailAccount
+    // const testMailJobs = ['welcome-email', 'forgot-password', 'new-comment', 'comment-reply']
+    // const testMailJob = testMailJobs[2]
+    // // cuenta a la cual le llegarán los emails
+    // const testMailAccount = 'bungew@gmail.com'
+    // // id usado para 'subscriber-update-X', tiene que estar en la DB
+    // const testMailUserId = '5e3c5cb34324aca40d6727d7'
+    // process.env.NOTIFICATIONS_MAILER_EMAIL = testMailAccount
 
-    log(`Mandando email de testeo a ${testMailAccount}`)
-    notifier.now(testMailJob, {
-      topic: {
-        id: '5e668613024049422bb22078',
-        mediaTitle: 'Direcciones y nombres de pasillos para el Barrio Las Flores',
-        authorName: 'Micaela Torres',
-        authorEmail: testMailAccount,
-        subscriber: testMailUserId,
-        // para new-proposal
-        nombre: '<Nombre y apellido>',
-        documento: '<Documento>',
-        telefono: '<Teléfono>',
-        email: '<Email>',
-        barrio: '<Barrio>',
-        tags: ['<Tag 1>', '<Tag 2>'],
-        problema: '<Problema>',
-        solucion: '<Solución>',
-        beneficios: '<Beneficios>'
-      },
-      userName: {
-        firstName:'<Suscriptorx>',
-        email: testMailAccount
-      },
-      // para welcome-email
-      to: testMailAccount,
-      validateUrl: 'https://validateUrl'
-    }).then(()=> {
-      // solo queremos testear el mail, no levantar el site
-      throw new Error('¡Error para que no continuar! Solo queremos probar los mails.')
-    })
+    // log(`Mandando email de testeo a ${testMailAccount}`)
+    // notifier.now(testMailJob, {
+    //   topic: {
+    //     id: '5e668613024049422bb22078',
+    //     mediaTitle: 'Direcciones y nombres de pasillos para el Barrio Las Flores',
+    //     authorName: 'Micaela Torres',
+    //     authorEmail: testMailAccount,
+    //     subscriber: testMailUserId,
+    //     // para new-proposal
+    //     nombre: '<Nombre y apellido>',
+    //     documento: '<Documento>',
+    //     telefono: '<Teléfono>',
+    //     email: '<Email>',
+    //     barrio: '<Barrio>',
+    //     tags: ['<Tag 1>', '<Tag 2>'],
+    //     problema: '<Problema>',
+    //     solucion: '<Solución>',
+    //     beneficios: '<Beneficios>'
+    //   },
+    //   userName: {
+    //     firstName:'<Suscriptorx>',
+    //     email: testMailAccount
+    //   },
+    //   // para welcome-email
+    //   to: testMailAccount,
+    //   validateUrl: 'https://validateUrl'
+    // }).then(()=> {
+    //   // solo queremos testear el mail, no levantar el site
+    //   throw new Error('¡Error para que no continuar! Solo queremos probar los mails.')
+    // })
 
   }).catch((err) => {
     console.error('Error loading ext/lib/notifier: ', err)
