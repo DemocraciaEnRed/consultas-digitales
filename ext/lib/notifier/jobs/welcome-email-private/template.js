34d643cc3bd96a295671030e7a2d282f29b4859f
Guillermo Croppi <guillermocroppi@gmail.com>
2020-09-08 18:45:55 -0300

    Ahora en signup component si allowPublicSignUp es falso, hay cambios de textos y un boton para crear contraseñas al azar ademas de un mensaje de exito nuevo

--- ext/lib/notifier/jobs/welcome-email-private/template.js
+++ ext/lib/notifier/jobs/welcome-email-private/template.js
@@ -18,12 +18,12 @@ module.exports = ({
   body: html`
     <p>Hola <strong>${userName}</strong>,</p>
     <p>Te damos la bienvenida a <strong>${config.organizationName}</strong>. Han hecho tu registro para que comiences a participar.</p>
-    <p>Para ingresar, accede desde tu email y con la siguiente contraseña para acceder: ${password}</p>
+    <p>Para ingresar, accede entrando a la plataforma e ingresando tu dirección de correo y la siguiente contraseña: <strong>${password}</strong></p>
     ${buttonTemplate({
       url: baseUrl,
       text: 'Entrar a la plataforma'
     })}
-    <p style='font-size:16px'><strong>Te pedimos que la primera acción que hagas sea cambiar la contraseña entrando a tu perfil.</strong></p>
+    <p style='font-size:16px'><strong>¡Te pedimos que la primera acción que hagas sea cambiar la contraseña entrando a tu perfil!</strong></p>
     <p>Muchas gracias, ¡Te esperamos!</p>
     <p style='font-size:12px'><i>Si el botón de "Entrar a la plataforma" botón no funciona, copiá y pegá el siguiente link en tu navegador: <a href="${baseUrl}" target="_blank">${baseUrl}</a></i></p>
   `
